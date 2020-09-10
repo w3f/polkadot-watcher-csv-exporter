a4edce72f98d23b3d3e72bc348b911c67d7f4881
ironoa <alessio.onori@gmail.com>
2020-09-10 14:46:02 +0200

    secret is now tested

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -22,6 +22,7 @@ main(){
     /scripts/build-helm.sh \
         --set environment=ci \
         --set image.tag="${CIRCLE_SHA1}" \
+        --set secret.gcpServiceAccountValue='{"type":"service_account","project_id":"test","private_key_id":"xxxxx","private_key":"-----BEGIN PRIVATE KEY-----\nxxxxx","client_email":"xxxx","client_id":"xxxx","auth_uri":"xxx","token_uri":"xxxx","auth_provider_x509_cert_url":"xxxx","client_x509_cert_url":"xxxxx"}' \
         polkadot-watcher-csv-exporter \
         ./charts/polkadot-watcher-csv-exporter
 
